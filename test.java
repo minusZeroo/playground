f8d794b59499433d7ec1d1a64b34c8cdc0d99780
Paul Jideani <pcijideani@gmail.com>
2024-05-26 20:48:03 +0200

    refactor

--- test.java
+++ test.java
@@ -4,9 +4,9 @@ package librarysystem;
 public class methodsearchBook {
 
     public class LibrarySystem {
-        public static void main(String[] args){
+        public static void main(String[] args) {
 
-            Book[]books=new Book[5];
+            Book[] books = new Book[5];
 
             books[0]=new Book("Java programming","John Smith");
             books[1]=new Book("Data Structure","Alice Johnson");
@@ -14,10 +14,10 @@ public class methodsearchBook {
             books[3]=new Book("Algorithms","Emily Davis");
             books[4]=new Book("Web development","Michael Brown");
         }
-        public static int searchBook(Book[]books, String targetTitle){
+        public static int searchBook(Book[] books, String targetTitle){
 
           for (int i = 0; i < books.length; i++){
-              if(books[i].getTitle().equals(targetTitle)){
+              if (books[i].getTitle().equals(targetTitle)) {
                   return i;//Return the index if the book is found
               }
           }
