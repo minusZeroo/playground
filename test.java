82dc6635f75d71ee9883f9a2e8dad3eae37281b1
Paul Jideani <pcijideani@gmail.com>
2024-05-26 20:46:51 +0200

    refactor

--- test.java
+++ test.java
@@ -1,18 +1,13 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package librarysystem;
 
-/**
- *
- * @author llama
- */
+
 public class methodsearchBook {
-   
-    public class LibrarySystem{
+
+    public class LibrarySystem {
         public static void main(String[] args){
+
             Book[]books=new Book[5];
+
             books[0]=new Book("Java programming","John Smith");
             books[1]=new Book("Data Structure","Alice Johnson");
             books[2]=new Book("Machine Learning","David Williams");
@@ -20,13 +15,14 @@ public class methodsearchBook {
             books[4]=new Book("Web development","Michael Brown");
         }
         public static int searchBook(Book[]books, String targetTitle){
-          for(int i=0; i<books.length; i++){
+
+          for (int i = 0; i < books.length; i++){
               if(books[i].getTitle().equals(targetTitle)){
                   return i;//Return the index if the book is found
               }
           }
           return -1;//Return -1 if the book is not found
-              }
+            }
             
         }  
         
